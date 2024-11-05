feee99340bb421e89aa2fcb3af5dda119ec83dbd
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-05 17:53:36 +0800

    intialize schema and cleaned up page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { UserButton } from "@/components/user-button";
-import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-
   return (
     <div>
-      Logged in!
       <UserButton />
     </div>
   );
