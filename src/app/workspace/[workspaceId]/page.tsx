a1f64383b1d88414ad9d05ed0e968612a8e5c301
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-09 23:16:03 +0800

    initialize toolbar feat

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useParams } from "next/navigation";
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
 
-  return <div>ID: {workspaceId}</div>;
+  return <div>Data: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
