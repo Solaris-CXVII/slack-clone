12314bb329f08d369a3e0f5143f24f29cf06c80e
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-10 23:12:42 +0800

    feat loading indicator and workspace display for workspaceswitcher.tsx

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,5 +1,9 @@
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { Button } from "@/components/ui/button";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,11 +11,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const WorkspaceSwitcher = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
+  const [_open, setOpen] = useCreateWorkspaceModal();
 
   const { data: workspaces, isLoading: workspacesLoading } = useGetWorkspaces();
 
@@ -27,14 +33,23 @@ export const WorkspaceSwitcher = () => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
-          A
+          {workspaceLoading ? (
+            <Loader className="size-5 animate-spin shrink-0" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent
-        side="bottom"
-        align="start"
-        className="w-64"
-      ></DropdownMenuContent>
+      <DropdownMenuContent side="bottom" align="start" className="w-64">
+        {" "}
+        <DropdownMenuItem
+          onClick={() => router.push(`/workspace/${workspaceId}`)}
+          className="cursor-pointer flex-col justify-start items-start capitalize"
+        >
+          {workspace?.name}
+          <span className="text-xs txt-muted-foreground">Active workspace</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
