e545f622677ed861d2f549b6c05f9eb64f6da50d
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-12 23:33:18 +0800

    feat add loading state and data fetching to workspace-sidebar

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,12 +1,25 @@
 import { UseCurrentMember } from "@/features/auth/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Loader } from "lucide-react";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
 
-  const { } = UseCurrentMember();
-  const { } = useGetWorkspace();
+  const { data: member, isLoading: memberLoading } = UseCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
 
   return <div>Workspace Sidebar</div>;
 };
