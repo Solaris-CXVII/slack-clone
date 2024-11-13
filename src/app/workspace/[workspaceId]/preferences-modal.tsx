e77c4c968b410dbb45ca1a69df7e8d212410559f
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-13 22:02:31 +0800

    style enhance preference modal layout and add workspace name section

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -25,10 +25,21 @@ export const PreferencesModal = ({
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
+      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+        <DialogHeader className="p-4 border-b bg-white">
           <DialogTitle>{value}</DialogTitle>
         </DialogHeader>
+        <div className="px-4 pb-4 flex flex-col gapy-y-2">
+          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-semibold">Workspace name</p>
+              <p className="text-sm text-[#1264A3] hover:underline font-semibold">
+                Edit
+              </p>
+            </div>
+            <p className="text-sm">{value}</p>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
