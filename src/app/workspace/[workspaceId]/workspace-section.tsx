ec083e2e8305bedcc112698e43c4e8a371b11e0d
Solaris-CXVII <aldencua2403@gmail.com>
2024-11-17 23:38:45 +0800

    feat create collapsible WorkspaceSection component for sidebar sections

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { FaCaretDown } from "react-icons/fa";
+
 interface WorkspaceSectionProps {
   children: React.ReactNode;
   label: string;
@@ -11,5 +14,24 @@ export const WorkspaceSection = ({
   hint,
   onNew,
 }: WorkspaceSectionProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col mt-3 px-2">
+      <div className="flex items-center px-3.5 group">
+        <Button
+          variant="transparent"
+          className="p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6"
+        >
+          <FaCaretDown className="size-4" />
+        </Button>
+        <Button
+          variant="transparent"
+          size="sm"
+          className="group px-1.5 text-sm text-[#f9edffcc] h-[28px] justify-start overflow-hidden items-center"
+        >
+          <span className="truncate">{label}</span>
+        </Button>
+      </div>
+      {children}
+    </div>
+  );
 };
